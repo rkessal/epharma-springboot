d7c7f6a13a74ba6775a3798debac22f1ccfafb63
Florent DAGNOT <florent.dagnot@laposte.net>
2023-02-20 17:03:11 +0100

    init commit

---
+++ src/main/java/hn/epharma/EpharmaApplication.java
@@ -0,0 +1,13 @@
+package hn.epharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpharmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpharmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hn/epharma/EpharmaApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.epharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpharmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
